3c7de312a6430bb42b4ab9a28bb3aa7204f92707
Aditya <adityateotia59@gmail.com>
2025-12-31 13:13:58 +0530

    Coonected to database

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
-const {adminAuth} = require("./Middlewares/auth");
-app.get("/admin/getAllData", adminAuth , (req,res)=>{
-    res.send("All data sent");
+// error handling
+// app.get("/user",(req,res)=>{
+//     // suppose there is any error in the code
+//     throw new Error("tewjvjk  bqj najwnfjb efbubjksabwiuf ");
+//     res.send("This is the error handling portion");
+    
+// })
+// app.use("/",(err,req,res,next)=>{
+//     if(err){
+//         res.status(500).send("Something went wrong");
+
+//     }
+// });
+//Second way to handle this error always use try and catch block
+app.use("/user",(req,res)=>{
+    try{
+       throw new Error("tewjvjk  bqj najwnfjb efbubjksabwiuf ");
+        res.send("This is the error handling portion");
+    }catch(err){
+        res.status(500).send("Something went wrong please conatct the support team");
+    }
 });
-app.delete("/admin/deleteData",adminAuth,(req,res)=>{
-    res.send('Deleted the data');
+//this will run when we throw an error but we are handling it inside try catch block if you do not use it it will run
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Something went wrong");
+
+    }
 });
 
 app.listen(3000,()=>{
