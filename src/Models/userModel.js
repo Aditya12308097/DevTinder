c38681d7d210f1fab1f9e702ce1a67c0e2f04e5c
Aditya <adityateotia59@gmail.com>
2026-01-04 11:32:21 +0530

    Adding Validations to our fields at API level and Database Level

--- src/Models/userModel.js
+++ src/Models/userModel.js
@@ -1,23 +1,78 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+
+
 const userSchema = mongoose.Schema({
     firstName : {
-        type : String
+        type : String,
+        required : true,
+        minLength : 4,
+        maxLength : 50,
+        validate(value){
+            if(validator.isEmpty(value)){
+                throw new Error("Can't leave blank firstName");
+            }
+        }
     },
     lastName :{
-        type: String
+        type: String,
+        required : true
     },
     emailId :{
-        type : String
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Please enter the correct Email Address");
+            }
+        }
     },
     password :{
-        type : String
+        type : String,
+        required : true,
+        minLength : 6,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Password must contain atleast 1 lowercase letter , 1 upparcase letter, 1 symbol and minimum length must be 8");
+            }
+        }
     },
     age : {
-        type : Number
+        type : Number,
+        min : 18,
+        max : 80
     },
     gender : {
-        type : String
+        type : String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender Data is not Valid");
+            }
+        }
+
+    },
+    skills :{
+        type : [String]
+    },
+    photo : {
+        type : String,
+        default : "This is the URL of the image",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Please enter correct URL");
+            }
+        }
+    },
+    about : {
+        type : String,
+        default : "I am a Software Developer"
     }
+
+},{
+    timestamps : true
 });
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ const connectDB = require("./config/database");
 const User = require("./Models/userModel");
 const app = express();
 app.use(express.json());
-app.delete("/user",async (req,res)=>{
-    const count = await User.deleteOne({firstName : req.body.firstName});
-    try{console.log(count);
-    res.send("Deleteed User successfully");
-    }catch(err){
-        res.status(400).send("Something went wrong");
-    }
-})
-app.delete("/user/id", async (req,res) =>{
+// app.delete("/user",async (req,res)=>{
+//     const count = await User.deleteOne({firstName : req.body.firstName});
+//     try{console.log(count);
+//     res.send("Deleteed User successfully");
+//     }catch(err){
+//         res.status(400).send("Something went wrong");
+//     }
+// })
+app.delete("/user", async (req,res) =>{
     const userId = req.body.userId;
     try{
         await User.findByIdAndDelete(userId);
@@ -21,16 +21,29 @@ app.delete("/user/id", async (req,res) =>{
         res.status(400).send("Something went wrong");
     }
 });
-app.patch("/user", async (req,res)=>{
-    const userId = req.body.userId;
+app.patch("/user/:userId", async (req,res)=>{
     const data = req.body;
-    console.log(req.body);
+    const userId = req.params?.userId;
+
     try{
-        const user = await User.findByIdAndUpdate({_id : userId}, data ,{returnDocument : "after"});
-        console.log(user);
+        const Allowed_updates = ["age","gender","skills","photo","password"];
+        const isAllowedUpdates = Object.keys(data).every((k)=>{
+                Allowed_updates.includes(k);
+        });
+        if(!isAllowedUpdates){
+            throw new Error("Do not update so much data");
+        }
+        if(data?.skills.length > 10){
+            throw new Error("Can't Store too much skills minimum is 10");
+        }
+        const user = await User.findByIdAndUpdate({_id: userId},data,{
+            returnDocument : "after",
+            runValidators : true
+        })
+        // console.log(user);
         res.send("User Updated Successfully");
     }catch(err){
-        res.status(400).send("Something went wrong");
+        res.status(400).send("Something went wrong"+err.message);
     }
 })
 app.get("/user",async (req,res)=>{
@@ -55,21 +68,29 @@ app.get("/feed", async (req,res)=>{
         res.status(400).send("Something went wrong");
     }
 })
-app.get("/findById",async (req,res)=>{
-    try{
-        const user = await User.findById({ _id :req.body._id});
-        if(!user){
-            res.status(401).send("User not found");
-        }else{
-            res.send(user)
-        }
-    }catch(err){
-        res.status(400).send("Something went wrong : "+ err.message);
-    }
-})
-app.post("/signup", async (req, res) => {
+// app.get("/findById",async (req,res)=>{
+//     try{
+//         const user = await User.findById({ _id :req.body._id});
+//         if(!user){
+//             res.status(401).send("User not found");
+//         }else{
+//             res.send(user)
+//         }
+//     }catch(err){
+//         res.status(400).send("Something went wrong : "+ err.message);
+//     }
+// })
+app.post("/user", async (req, res) => {
     // console.log(req.body);
     try{
+        const email = req.body.emailId;
+        const userExist = await User.findOne({emailId : email});
+        if(userExist){
+            throw new Error("User Exists, try with different Email Id ");
+        }
+        if(req.body.skills.length > 10){
+            throw new Error("Can't store too much skills");
+        }
         const user = new User(req.body);
         await user.save();//this command will save the data inside the User collection
         res.send("User data successfully saved into database");
@@ -78,21 +99,21 @@ app.post("/signup", async (req, res) => {
     }
   
 });
-app.post("/dummyDataSignup", async (req,res)=>{
-    try{
-        const userData = {
-            firstName : "Govind Singh",
-            lastName : "Teotia",
-            emailId : "govindSingh@gmail.com",
-            password : "Govind@123",
-        };
-        const user = new User(userData);
-        await user.save();
-        res.send("Data saved successfully");
-    }catch(err){
-        res.status(400).send("Something went wrong");
-    }
-})
+// app.post("/dummyDataSignup", async (req,res)=>{
+//     try{
+//         const userData = {
+//             firstName : "Govind Singh",
+//             lastName : "Teotia",
+//             emailId : "govindSingh@gmail.com",
+//             password : "Govind@123",
+//         };
+//         const user = new User(userData);
+//         await user.save();
+//         res.send("Data saved successfully");
+//     }catch(err){
+//         res.status(400).send("Something went wrong");
+//     }
+// })
 connectDB()
 .then(()=>{
     console.log("Connected to the Database");
